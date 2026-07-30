1710f209a18dbf6b81c959f4adeccf99011d93ab
Priya-N2526 <npriya2526@gmail.com>
2026-07-30 13:00:44 +0530

    day1

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,7 @@ public class WelcomeToJava {
         System.out.println("Welcome to the java track");
         System.out.println("I Already know how to build programming logic.");
         System.out.println("Now i am learning how to express that logic using java.");
+        System.out.println("Now I know how to use git and github");
+
     }
 }
