0a1eac889e9f4d9ead9eaa73aa85b34fcaa46fa6
Priya-N2526 <npriya2526@gmail.com>
2026-08-03 15:58:23 +0530

    day 2 p2

--- track/M02/T01/SD01/PO1/Main.java
+++
@@ -1,7 +0,0 @@
-package track.M02.T01.SD01.PO1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello KodNest");
-    }
-}
---
+++ track/M02/T01/ST01/PO1/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Write your code here.
+        String learnerName = "Ananya";
+        int completedProblems = 25;
+        double practiceHours = 2.5;
+        boolean projectedCompleted = true;
+
+        System.out.println("Learner: " + learnerName);
+        System.out.println("Problems Completed: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectedCompleted);
+    }
+}
