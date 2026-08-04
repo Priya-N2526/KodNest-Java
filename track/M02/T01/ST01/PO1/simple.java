43d00a71e974d5e0f233a80cbb75c436501c568e
Priya-N2526 <npriya2526@gmail.com>
2026-08-04 12:26:06 +0530

    Day3 P2

--- track/M02/T01/ST01/PO1/simple.java
+++ track/M02/T01/ST01/PO1/simple.java
@@ -1,11 +1,28 @@
-public class simple{
-    public static void main(String[] args){
-        System.out.println("Monday");
-        System.out.print("Tuesday");
-        System.out.println("Wednesday");
-        System.out.println("Thursday");
-        System.out.print("Friday"):
-        System.out.println("Saturday");
-        System.out.print("Sunday");
+public class simple {
+    public static void main(String[] args) {
+        double Principal = 10000.0;
+        double rate = 6.5;
+        double time = 2.0;
+        double weight = 72.0;
+        double height = 1.8;
+
+        int s1 = 78;
+        int s2 = 84;
+        int s3 = 69;
+        int s4 = 91;
+        int s5 = 88;
+
+        int totalMarks = s1 + s2 + s3 + s4 + s5;
+
+        double simpleInterest = Principal * rate * time / 100.0;
+        double totalAmount = Principal + simpleInterest;
+        double bmi = weight / (height * height);
+        double percentage = totalMarks * 100.0 / 500;
+
+        System.out.println("Simple Interest: " + simpleInterest);
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("BMI: " + bmi);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage);
     }
 }
---
+++ track/M03/T01/number.java
@@ -0,0 +1,16 @@
+package track.M03.T01;
+
+public class number {
+    public static void main(String[] args){
+        int num=-9;
+        if(num>0){
+            System.out.println("Positive");
+        }
+        else if(num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.err.println("Zero");
+        }
+    }
+}
