dcfeb6891a90c7454e0c4f458517b5ad528b5113
Priya-N2526 <npriya2526@gmail.com>
2026-08-17 12:28:21 +0530

    po1

---
+++ track/M02/T01/ST01/arrays/Main.java
@@ -0,0 +1,19 @@
+package track.M02.T01.ST01.arrays;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("enter the Array elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array elements are:  ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+            
+        }
+    }
+    
+}
